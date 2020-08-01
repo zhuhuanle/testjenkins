4e196523ed044b861bcaa5b8a4f583bae517da82
zhuhuanle <happy_zhu@163.com>
2020-08-01 14:28:52 +0800

    第一次上传

---
+++ src/main/java/com/test/jenkins/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.test.jenkins.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : zhu_huan
+ * @createTime : 2020/8/1
+ **/
+@RestController
+public class TestController {
+
+    @GetMapping("/get")
+    public String test(){
+        return "hello jenkins";
+    }
+}
